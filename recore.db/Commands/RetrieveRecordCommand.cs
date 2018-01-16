d06f02eb8566a87a76fc405ac42e63aa5487b642
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2018-01-16 18:31:23 -0500

    Added definition of RetrieveRecord commands

---
+++ recore.db/Commands/RetrieveRecordCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace recore.db.Commands
+{
+    public class RetrieveRecordCommand: CommandBase
+    {
+        public string Type
+        {
+            get => (string)this["Type"];
+            set => this["Type"] = value;
+        }
+        public Guid Id
+        {
+            get => (Guid)this["Id"];
+            set => this["Id"] = value;
+        }
+        public List<string> Fields
+        {
+            get => (List<string>)this["Fields"];
+            set => this["Fields"] = value;
+        }
+        public bool AllFields
+        {
+            get => (bool)this["AllFields"];
+            set => this["AllFields"] = value;
+        }
+    }
+}
---
+++ recore.db/Commands/RetrieveRecordResult.cs
@@ -0,0 +1,11 @@
+﻿namespace recore.db.Commands
+{
+    public class RetrieveRecordResult: ResultBase
+    {
+        public Record Result
+        {
+            get => (Record)this["Result"];
+            set => this["Result"] = value;
+        }
+    }
+}
